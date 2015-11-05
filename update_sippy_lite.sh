742a550d691c801b3fc0af95844858abf95bcfd9
Maksym Sobolyev <sobomax@sippysoft.com>
2015-11-05 13:04:36 -0800

    Use branch -D, not branch -d to remove temporary branch.

--- update_sippy_lite.sh
+++ update_sippy_lite.sh
@@ -4,7 +4,7 @@ set -e
 
 git checkout sippy_git_master
 git pull
-git branch -d sippy_git_master_toplevel
+git branch -D sippy_git_master_toplevel
 git subtree split --prefix=sippy --branch=sippy_git_master_toplevel
 git checkout master
 git pull
