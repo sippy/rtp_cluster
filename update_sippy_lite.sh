5cc301360b9ef24d6c182f54cbcce3ac91256793
Maksym Sobolyev <sobomax@sippysoft.com>
2016-10-04 18:11:15 -0700

    Extend update_sippy_lite.sh to move files around as needed.

--- update_sippy_lite.sh
+++ update_sippy_lite.sh
@@ -6,6 +6,15 @@ git checkout sippy_git_master
 git pull
 git branch -D sippy_git_master_toplevel
 git subtree split --prefix=sippy --branch=sippy_git_master_toplevel
+git checkout sippy_git_master_toplevel
+mkdir -p sippy_lite/Math
+mkdir -p sippy_lite/Time
+for f in `cat sippy_lite.list`
+do
+  git mv ${f} sippy_lite/${f}
+done
+git rm .gitignore dictionary *.py
+false
 git checkout master
 git pull
 git merge sippy_git_master_toplevel
