80b7f538d948840e3c097ec643a0ac897a33a8d8
Maksym Sobolyev <sobomax@sippysoft.com>
2016-10-05 18:40:11 -0700

    o read sippy_lite.list at the start, it might be gone when we do checkout etc.
    o remove debug trap.

--- update_sippy_lite.sh
+++ update_sippy_lite.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+SLITE_LIST=`cat sippy_lite.list`
 git checkout sippy_git_master
 git pull
 git branch -D sippy_git_master_toplevel
@@ -9,14 +10,13 @@ git subtree split --prefix=sippy --branch=sippy_git_master_toplevel
 git checkout sippy_git_master_toplevel
 mkdir -p sippy_lite/Math
 mkdir -p sippy_lite/Time
-for f in `cat sippy_lite.list`
+for f in ${SLITE_LIST}
 do
   git mv ${f} sippy_lite/${f}
 done
 git rm .gitignore dictionary *.py
-false
 git checkout master
 git pull
 git merge sippy_git_master_toplevel
-git commit
-git push
+#git commit
+#git push
