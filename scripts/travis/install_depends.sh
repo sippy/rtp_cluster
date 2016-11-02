3e0cd36861ad489dce167e836793b06c1287c476
Maksym Sobolyev <sobomax@sippysoft.com>
2016-11-02 15:03:40 -0700

    If RTPP_BRANCH != master pull rtpproxy master's sources in the separate dir so that we can use some tools from there.

--- scripts/travis/install_depends.sh
+++ scripts/travis/install_depends.sh
@@ -17,6 +17,13 @@ cd "${DISTDIR}"
 RTPP_BRANCH=${RTPP_BRANCH:-"master"}
 MAKE_CMD="make"
 git clone -b "${RTPP_BRANCH}" --recursive git://github.com/sippy/rtpproxy.git
+
+if [ "${RTPP_BRANCH}" != "master" ]
+then
+  git clone -b master --recursive git://github.com/sippy/rtpproxy.git \
+   "${RTPPDDIR_m}"
+fi
+
 cd rtpproxy
 ./configure
 ${MAKE_CMD} all
